c59add4648e60421b0c2f317fd0a99a303e9c669
Oleksandr-Yeremenko <AleksantdrEremenko1988@gmail.com>
2021-08-29 17:13:34 +0300

    fix remark

--- app/src/main/java/com/example/lotteryjava/MainActivity.java
+++ app/src/main/java/com/example/lotteryjava/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.content.res.Resources;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +22,14 @@ public class MainActivity extends AppCompatActivity {
         TextView message = (TextView)findViewById(R.id.message);
         ImageView bulb = (ImageView)findViewById(R.id.bulb);
 
+        calculation(buttonCheck, edit, message, bulb);
+    }
+
+    public void calculation(Button buttonCheck, EditText edit, TextView message, ImageView bulb) {
         buttonCheck.setOnClickListener(new View.OnClickListener () {
+            public String textInvalidTicket = getString(R.string.invalidLotteryTicket);
+            public String textEmptyMessage = getString(R.string.emptyMessage);
+
             @Override
             public void onClick(View view) {
                 int firstPartTicket = 0;
@@ -29,23 +37,30 @@ public class MainActivity extends AppCompatActivity {
                 String numberTicket = edit.getText().toString();
 
                 if (numberTicket.length() != 6) {
-                    message.setText("Не вірний номер");
+                    message.setText(textInvalidTicket);
                 } else {
-                    for (int y = 0; y < 3; y++) {
-                        firstPartTicket = firstPartTicket + Character.getNumericValue(numberTicket.charAt(y));
-                    }
+                    message.setText(textEmptyMessage);
 
-                    for (int y = 3; y < 6; y++) {
-                        secondPartTicket = secondPartTicket + Character.getNumericValue(numberTicket.charAt(y));
-                    }
-
-                    if (firstPartTicket == secondPartTicket) {
-                        bulb.getBackground().setColorFilter(0xff00ff00, PorterDuff.Mode.MULTIPLY);
+                    if (equalityTest(firstPartTicket, secondPartTicket, numberTicket)) {
+                        bulb.getBackground().setColorFilter(getResources().getColor(R.color.green), PorterDuff.Mode.MULTIPLY);
+                        //bulb.getBackground().setColorFilter(0xff00ff00, PorterDuff.Mode.MULTIPLY);
                     } else {
-                        bulb.getBackground().setColorFilter(0xffff0000, PorterDuff.Mode.MULTIPLY);
+                        bulb.getBackground().setColorFilter(getResources().getColor(R.color.red), PorterDuff.Mode.MULTIPLY);
                     }
                 }
             }
         });
+    }
+
+    public boolean equalityTest(int firstPartTicket,int secondPartTicket,String numberTicket) {
+        for (int y = 0; y < 3; y++) {
+            firstPartTicket = firstPartTicket + Character.getNumericValue(numberTicket.charAt(y));
+        }
+
+        for (int y = 3; y < 6; y++) {
+            secondPartTicket = secondPartTicket + Character.getNumericValue(numberTicket.charAt(y));
+        }
+
+        return (firstPartTicket == secondPartTicket);
     }
 }
